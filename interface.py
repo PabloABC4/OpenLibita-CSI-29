a40d05203c26fcac5419519875b674be0f2b9700
Marcelo <marceloloiolaveras@gmail.com>
2024-11-12 01:44:26 -0300

    fix: now user may only remove existent Book IDs

--- interface.py
+++ interface.py
@@ -76,12 +76,25 @@ def remove_book():
             messagebox.showerror("Erro", "ID do Livro é obrigatório.")
             return
 
+        if book_id not in [str(book[0]) for book in books]:
+            messagebox.showerror("Erro", "ID de Livro não encontrado.")
+            return
+
         result = backend.remove_book(book_id)
         if isinstance(result, str):
             messagebox.showerror("Erro", result)
         else:
             messagebox.showinfo("Sucesso", "Livro removido com sucesso.")
             remove_book_window.destroy()
+
+    books = backend.get_books()
+    if isinstance(books, str):
+        messagebox.showerror("Erro", books)
+        return
+
+    if not books:
+        messagebox.showinfo("Info", "Nenhum livro encontrado no banco de dados.")
+        return
 
     remove_book_window = Toplevel(root)
     remove_book_window.title('Remover Livro')
@@ -92,6 +105,9 @@ def remove_book():
     book_id_entry.grid(column=1, row=0, padx=10, pady=5)
 
     ttk.Button(remove_book_window, text="Enviar", command=submit_removal).grid(column=0, row=1, columnspan=2, pady=10)
+
+    ttk.Button(remove_book_window, text="Sair", command=remove_book_window.destroy).grid(column=1, row=2, columnspan=2, padx=10, pady=10, stick=S)
+
 
 root = Tk()
 root.title('Sistema de Gerenciamento de Biblioteca')
